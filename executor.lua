05c78285ca5ae7c18374323c1b98d0d95a1c855a
jdev <72287871+jdev-coder@users.noreply.github.com>
2023-01-11 18:42:57 -0800

    Update executor.lua

--- executor.lua
+++ executor.lua
@@ -69,7 +69,7 @@ function Importer:ImportFromRepo(repo_author, repo_name, branch)
     }
 end
 --#endregion
-local importer = Importer:ImportFromRepo("sound-infinity", "Cheat-Engine", "master")
+local importer = Importer:ImportFromRepo("jdev-coder", "Cheat-Engine", "master")
 local function require(pathname)
     pathname = pathname:gsub("[.]", "/")
     pathname = pathname .. ".lua"
